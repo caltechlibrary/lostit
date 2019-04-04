97ab50a2d2ba5aed8542ef2c6b554304509b0012
Michael Hucka <mhucka@caltech.edu>
2019-04-04 11:28:56 -0700

    Add initial init and version files

---
+++ lostit/__version__.py
@@ -0,0 +1,17 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   Lost It! version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/caltechlibrary/lostit
+# =============================================================================
+
+__version__     = '0.0.1'
+__title__       = 'lostit'
+__name__        = 'Lost It!'
+__description__ = '''A program for Caltech Library's Circulation team to easily report lost items.'''
+__url__         = 'https://github.com/caltechlibrary/lostit'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'mhucka@caltech.edu'
+__license__     = 'BSD 3-clause license -- see LICENSE file'
+__copyright__   = 'Copyright (C) 2018 by the California Institute of Technology'
