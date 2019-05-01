0c8fed7503933ca23438f1a33048bb2d78f6fb45
Michael Hucka <mhucka@caltech.edu>
2019-05-01 15:36:17 -0700

    Add more exceptions for net()

--- lostit/exceptions.py
+++ lostit/exceptions.py
@@ -22,6 +22,17 @@ class ServiceFailure(Exception):
     '''Unrecoverable problem involving network services.'''
     pass
 
+class NoContent(Exception):
+    '''Server returned a code 401 or 404, indicating no content found.'''
+
+class RateLimitExceeded(Exception):
+    '''The service flagged reports that its rate limits have been exceeded.'''
+    pass
+
 class InternalError(Exception):
     '''Unrecoverable problem involving Lost It! itself.'''
+    pass
+
+class RequestError(Exception):
+    '''Problem with the TIND query or request.'''
     pass
