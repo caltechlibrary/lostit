0a51ce2a9f720462d9f45cbff69b1bc0e525c39b
Michael Hucka <mhucka@caltech.edu>
2019-06-18 16:28:05 -0700

    Add mail support

--- lostit/__main__.py
+++ lostit/__main__.py
@@ -27,6 +27,7 @@ from lostit.access import AccessHandlerGUI, AccessHandlerCLI
 from lostit.config import Config
 from lostit.control import LostItControlGUI, LostItControlCLI
 from lostit.debug import set_debug, log
+from lostit.email import Mailer
 from lostit.exceptions import *
 from lostit.files import module_path
 from lostit.google_sheet import Google
@@ -46,6 +47,7 @@ from lostit.tind import Tind
     no_color   = ('do not color-code terminal output',              'flag',   'C'),
     no_gui     = ('do not start the GUI interface (default: do)',   'flag',   'G'),
     no_keyring = ('do not store credentials in a keyring service',  'flag',   'K'),
+    no_mail    = ('do not send mail (default: do)',                 'flag',   'M'),
     reset_keys = ('reset user and password used',                   'flag',   'R'),
     no_sheet   = ('do not open the spreadsheet (default: open it)', 'flag',   'S'),
     version    = ('print version info and exit',                    'flag',   'V'),
@@ -53,8 +55,8 @@ from lostit.tind import Tind
 )
 
 def main(user = 'U', pswd = 'P',
-         no_color = False, no_gui = False, no_keyring = False, no_sheet = False,
-         reset_keys = False, version = False, debug = False):
+         no_color = False, no_gui = False, no_keyring = False, no_mail = False,
+         no_sheet = False, reset_keys = False, version = False, debug = False):
     '''Lost It!'''
 
     # Our defaults are to do things like color the output, which means the
@@ -64,6 +66,7 @@ def main(user = 'U', pswd = 'P',
     use_keyring = not no_keyring
     use_gui     = not no_gui
     view_sheet  = not no_sheet
+    send_mail   = not no_mail
 
     # We use default values that provide more intuitive help text printed by
     # plac.  Rewrite the values to things we actually use.
@@ -98,16 +101,19 @@ def main(user = 'U', pswd = 'P',
 
     # Start the worker thread.
     if __debug__: log('starting main body thread')
-    controller.start(MainBody(view_sheet, debug, controller, accesser, notifier, tracer))
+    controller.start(MainBody(view_sheet, send_mail, debug,
+                              controller, accesser, notifier, tracer))
 
 
 class MainBody(Thread):
     '''Main body of Lost It! implemented as a Python thread.'''
 
-    def __init__(self, view_sheet, debug, controller, accesser, notifier, tracer):
+    def __init__(self, view_sheet, send_mail, debug,
+                 controller, accesser, notifier, tracer):
         '''Initializes main thread object but does not start the thread.'''
         Thread.__init__(self, name = "MainBody")
         self._view_sheet = view_sheet
+        self._send_mail  = send_mail
         self._debug      = debug
         self._controller = controller
         self._tracer     = tracer
@@ -122,6 +128,7 @@ class MainBody(Thread):
     def run(self):
         # Set shortcut variables for better code readability below.
         view_sheet = self._view_sheet
+        send_mail  = self._send_mail
         debug      = self._debug
         controller = self._controller
         accesser   = self._accesser
@@ -136,10 +143,14 @@ class MainBody(Thread):
 
         # Let's do this thing.
         try:
-            config = Config(path.join(module_path(), "lostit.ini"))
-            tind   = Tind(accesser, notifier, tracer)
-            google = Google(accesser, notifier, tracer)
-            sid    = config.get('lostit', 'spreadsheet_id')
+            config      = Config(path.join(module_path(), "lostit.ini"))
+            tind        = Tind(accesser, notifier, tracer)
+            google      = Google(accesser, notifier, tracer)
+
+            sid         = config.get('lostit', 'spreadsheet_id')
+            mail_server = config.get('lostit', 'mail_server')
+            mail_port   = config.get('lostit', 'mail_port')
+            recipients  = config.get('lostit', 'mail_recipients')
 
             # Get the data.
             tind_records = tind.records()
@@ -148,20 +159,34 @@ class MainBody(Thread):
             # Figure out what's new.
             tracer.update('Comparing TIND records to our spreadsheet')
             new_records = records_diff(google_records, tind_records)
-            tracer.update('Found {} new records'.format(len(new_records)))
+            num_new = len(new_records)
+            tracer.update('Found {} new records'.format(num_new))
 
             # Update the Google spreadsheet with new records.
-            if len(new_records) > 0:
+            if num_new > 0:
                 new_records = sorted(new_records, key = lambda r: r.date_requested)
                 tracer.update('Updating Google spreadsheet')
                 google.update(sid, new_records)
 
-            # Open the spreadsheet too, if requested.
+            # Open the spreadsheet, if requested.
             if isinstance(notifier, MessageHandlerGUI):
                 if notifier.yes_no('Open the tracking spreadsheet?'):
+                    tracer.update('Opening the Google spreadsheet')
                     google.open(sid)
             elif view_sheet:
+                tracer.update('Opening the Google spreadsheet')
                 google.open(sid)
+
+            # Send mail, if requested.
+            if send_mail and num_new > 0:
+                tracer.update('Sending mail')
+                subject  = 'Lost It! found {} lost items'.format(num_new)
+                body     = email_body(num_new, google.spreadsheet_url(sid))
+                sender   = accesser.user + '@caltech.edu'
+                password = accesser.password
+                mailer   = Mailer(mail_server, mail_port)
+                mailer.send(sender, password, recipients, subject, body)
+
         except (KeyboardInterrupt, UserCancelled) as err:
             tracer.stop('Quitting.')
             controller.stop()
@@ -179,16 +204,27 @@ class MainBody(Thread):
             tracer.stop('Done')
             controller.stop()
 
+
+# Miscellaneous utilities.
+# .............................................................................
 
-# On windows, we want the command-line args to use slash intead of hyphen.
+def email_body(num_new, sheet_url):
+    return '''
 
-if sys.platform.startswith('win'):
-    main.prefix_chars = '/'
+Lost It! was just run and it found {} new lost items.
+Here is the URL for the spreadsheet of lost items:
+{}
+    '''.format(num_new, sheet_url)
 
 
 # Main entry point.
 # ......................................................................
 # The following allows users to invoke this using "python3 -m lostit".
+
+if sys.platform.startswith('win'):
+    # When running on Windows, we want the command-line args to use the slash
+    # character intead of hyphen.
+    main.prefix_chars = '/'
 
 if __name__ == '__main__':
     plac.call(main)
